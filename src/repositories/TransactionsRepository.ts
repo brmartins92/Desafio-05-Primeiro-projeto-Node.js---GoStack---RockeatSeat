6a270e49c5e7b129b0c48c9e106def166ea62013
bruno pc desckop <brmartins92@gmail.com>
2020-05-14 00:03:24 -0300

    transction

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,27 +1,56 @@
 import Transaction from '../models/Transaction';
 
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
 }
 
+interface TransactionInterface {
+  title : string,
+  value : number,
+  type : 'income' | 'outcome'
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
-
+  
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce((accumulator: Balance,transaction: Transaction) =>  {
+      
+      if(transaction.type=="income"){
+        accumulator.income += transaction.value;
+        accumulator.total += transaction.value;
+      }else{
+        accumulator.outcome += transaction.value;
+        accumulator.total -= transaction.value;
+      }
+      return accumulator
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    })
+   
+
+    return balance;
   }
 
-  public create(): Transaction {
+  public create({title,value,type}: TransactionInterface ) : Transaction {
+    const transaction = new Transaction({ title,value,type });
+    this.transactions.push(transaction);
+    return transaction;
+
     // TODO
   }
 }
